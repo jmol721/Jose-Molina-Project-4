b118be410dfa1c5600dcaf87f2cff26117ac22be
jose <jmolina721@gmail.com>
2021-04-10 16:06:29 -0500

    Add timer: Created Penalty function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,42 @@
 // Timer componet
-var timeLeft = 2;
+var timeLeft = 15;
 var timeEl = document.getElementById('seconds');
 timeEl.textContent = timeLeft;
 
 
+// decrement timeLeft by 1 every second until 0 timeLeft
 function countdownTimer () {
     var timerInterval = setInterval(function() {
         if (timeLeft >= 0) {
             timeEl.textContent = timeLeft
             timeLeft--;
+            console.log("timeLeft :", timeLeft);
         }  else {
             clearInterval(timerInterval);
+            timeEl.textContent = 0;
             document.getElementById("timer").setAttribute("class", "timer0");
+            console.log("timeLeft :", timeLeft);
         }
     }, 1000);
-
 }
 
+// Decrement timeLeft by penalty
+function incorrectPenaltyTimer (penalty) {
+    timeLeft -= penalty;
+}
+
+
+
+//Quiz component
+var penalty = 2;
 
 // attaching quiz button to timer function
 var startQuizBtn = document.getElementById("start-button");
 
 startQuizBtn.onclick = countdownTimer;
+
+//Test button
+var wrongQuestion = document.getElementById("penalty-button");
+
+
+wrongQuestion.onclick = () => incorrectPenaltyTimer(penalty);
