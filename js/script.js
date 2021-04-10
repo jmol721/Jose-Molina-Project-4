438bc74cadbf958e8827ef5d69c1ff73f6bd00b2
jose <jmolina721@gmail.com>
2021-04-10 15:11:22 -0500

    Add timer: Created startTimer function/startTimer button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 // Timer componet
-var timeLeft = 100;
+var timeLeft = 2;
 var timeEl = document.getElementById('seconds');
 timeEl.textContent = timeLeft;
+
+
+function countdownTimer () {
+    var timerInterval = setInterval(function() {
+        if (timeLeft >= 0) {
+            timeEl.textContent = timeLeft
+            timeLeft--;
+        }  else {
+            clearInterval(timerInterval);
+            document.getElementById("timer").setAttribute("class", "timer0");
+        }
+    }, 1000);
+
+}
+
+
+// attaching quiz button to timer function
+var startQuizBtn = document.getElementById("start-button");
+
+startQuizBtn.onclick = countdownTimer;
