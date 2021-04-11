891bedfdb25ec837add24494f4fc6ef9bbcd4068
jose <jmolina721@gmail.com>
2021-04-11 14:56:49 -0500

    High Score: fixed go back button to use hide/show classes instead of refreshing page via html

--- js/script.js
+++ js/script.js
@@ -34,45 +34,69 @@ var startQuizBtn = document.getElementById("start-button");
 
 startQuizBtn.onclick = countdownTimer;
 
-//Test button
+
+// funciton for hiding and showing quiz container
+var hideQuizContainer = function() {
+    var quizContainerEl = document.getElementById('main-quiz-container-id');
+    quizContainerEl.className = 'hide';
+}
+
+var showQuizContainer = function() {
+    var quizContainerEl = document.getElementById('main-quiz-container-id');
+    quizContainerEl.className = "main-quiz-container";
+}
+
+// Testing
 // var wrongQuestion = document.getElementById("penalty-button");
-
-
 // wrongQuestion.onclick = () => incorrectPenaltyTimer(penalty);
 
 
 // High Score component
+var highScoreLink = document.getElementById("view-highscore-switch");
 var highScoreListCounter = 1;
-var highScoreList = document.querySelector("#high-score-list");
+var highScoreParentList = document.querySelector("#high-score-list");
+var highScoreList = [];
 
+var goBackButton = document.getElementById("go-back");
+var clearHighScoreButton = document.getElementById("clear-high-scores");
+
+// fucntion for displaying high score
+var showHighScore = function() {
+    hideQuizContainer();
+    var viewHighScoreEl = document.getElementById("view-high-score-id");
+    viewHighScoreEl.className = 'show';
+}
+
+var hideHighScore = function() {
+    var viewHighScoreEl = document.getElementById("view-high-score-id");
+    viewHighScoreEl.className = 'hide';
+    showQuizContainer();
+}
+
+highScoreLink.onclick = showHighScore;
+goBackButton.onclick = hideHighScore;
+
+
+// function to add new high score
 var createHighScoreEl = function(highScoreObject) {
     var highScoreListEl = document.createElement("li");
     highScoreListEl.className = "high-score-element";
     highScoreListEl.setAttribute("high-score-id", highScoreListCounter);
     highScoreListEl.textContent = `${highScoreListCounter}. ${highScoreObject.name} - ${highScoreObject.score}`;
 
-    highScoreList.appendChild(highScoreListEl);
+    highScoreParentList.appendChild(highScoreListEl);
+    highScoreListCounter++;
+
+    highScoreObject.id = highScoreListCounter;
+    highScoreList.push(highScoreObject);
+
+    console.log(highScoreList);
 }
 
-// Testing adding high score
-// var mockHighScore = {name: 'jose', score: 2};
-// createHighScoreEl(mockHighScore);
 
+// Testing
+var mockHighScore = {name: 'jose', score: 2};
+createHighScoreEl(mockHighScore);
 
-// funciton for hiding quiz container
-var hideQuizContainer = function() {
-    var quizContainerEl = document.getElementById('main-quiz-container-id');
-    quizContainerEl.className = 'hide';
-}
-
-// fucntion for displaying high score
-var displayHighScore = function() {
-    hideQuizContainer();
-    var viewHighScoreEl = document.getElementById("view-high-score-id");
-    viewHighScoreEl.className = 'show';
-    console.log('hello');
-}
-
-var highScoreLink = document.getElementById("view-highscore-switch");
-
-highScoreLink.onclick = displayHighScore;
+var mockHighScore2 = {name: 'poop', score: 100};
+createHighScoreEl(mockHighScore2);
